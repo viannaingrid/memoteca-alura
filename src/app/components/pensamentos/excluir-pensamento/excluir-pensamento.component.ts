4718d8653b80b4c4e071f023ab759b5a552d8d8d
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-05-07 13:38:21 -0300

    `Added ExcluirPensamentoComponent to declarations in app.module.ts`

---
+++ src/app/components/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excluir-pensamento',
+  templateUrl: './excluir-pensamento.component.html',
+  styleUrls: ['./excluir-pensamento.component.scss']
+})
+export class ExcluirPensamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
