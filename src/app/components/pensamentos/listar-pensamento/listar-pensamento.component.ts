083026b4870373b11bd9dab833564015e29a2ce6
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-05-07 09:24:48 -0300

    `Added input property to PensamentoComponent and updated ListarPensamentoComponent to pass pensamento object to PensamentoComponent instances.`

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = []
+  listaPensamentos = [
+    {
+      conteudo: 'Comunicação entre componentes',
+      autoria: 'Ingrid',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'I love Angular',
+      autoria: 'Dev Front',
+      modelo: 'modelo2'
+    }
+  ];
 
   constructor() { }
 
