d0d6d36e18222ca626cd41d73dc98f6118bf0f1c
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-05-07 14:06:02 -0300

    `Added ExcluirPensamentoComponent and related routes and services for deleting thoughts`

--- src/app/components/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
+++ src/app/components/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-excluir-pensamento',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExcluirPensamentoComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  pensamento:Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
   }
 
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
+      this.pensamento = pensamento
+    })
+  }
+
+  excluirPensamento(){
+    if(this.pensamento.id){
+      this.service.excluir(this.pensamento.id).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
+  }
+
+  cancelar(){
+    this.router.navigate(['/listarPensamento'])
+  }
 }
