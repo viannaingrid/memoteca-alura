294e2f257b0c4e84c2b38a528184c7c31f325ee6
Ingrid Oliveira <ngrid.vianna1@outlook.com>
2025-05-07 10:09:03 -0300

    `Added Pensamento interface and updated components to use it`

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -7,23 +8,7 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
-  listaPensamentos = [
-    {
-      conteudo: 'Comunicação entre componentes',
-      autoria: 'Ingrid',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'I love Angular',
-      autoria: 'Dev Front',
-      modelo: 'modelo2'
-    },
-    {
-      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc at bibendum tincidunt, magna erat tempor libero, nec accumsan nisl odio sit amet eros. Fusce nec quam ut justo tincidunt efficitur. Curabitur vel purus a orci pulvinar sagittis. Morbi porta, turpis nec suscipit consequat, velit lorem feugiat magna, sit amet dignissim enim sapien nec nunc. Vivamus commodo ante ut porta interdum.',
-      autoria: 'Dev Front',
-      modelo: 'modelo3'
-    },
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { }
 
---
+++ src/app/components/pensamentos/pensamento.ts
@@ -0,0 +1,7 @@
+export interface Pensamento {
+  //rules that define the structure of the object
+  id: number
+  conteudo: string
+  autoria: string
+  modelo: string
+}
